94932fc214f0fee4e58d658b4bba394841babf8c
miketully426 <fake@no.org>
2023-12-13 17:01:30 -0600

    initial commit

---
+++ src/main/java/org/launchcode/planets/models/Planet.java
@@ -0,0 +1,12 @@
+package org.launchcode.planets.models;
+
+public class Planet {
+
+    private String name;
+    private String diameter;
+    private String star;
+    private String distance;
+    private String image;
+    private int moons;
+
+}
